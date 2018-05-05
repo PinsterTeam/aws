80a711ac2841dfe51668a0584564d660c30831e2
scytherswings <scytherswings@gmail.com>
2018-05-04 23:19:31 -0400

    add elasticbeanstalk cli manually

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 1.15/Dockerfile -t cibuilds/aws:latest -t cibuilds/aws:1.15.14  -t cibuilds/aws:1.15 .
+docker build --file 1.15/Dockerfile -t pinster/awseb:latest -t pinster/awseb:1.15.14  -t pinster/awseb:1.15 .
